f2098e0a68f4d8ff08dec342873ab4dc75cdde97
YacoobSheriff <sheriffyacoob19@gmail.com>
2026-01-20 15:43:57 +0530

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,13 @@
+# functions.py
+# Function examples
+
+def greet(name):
+    print("Hello", name)
+
+greet("Sheriff")
+
+def add(a, b):
+    return a + b
+
+result = add(10, 5)
+print("Sum:", result)
