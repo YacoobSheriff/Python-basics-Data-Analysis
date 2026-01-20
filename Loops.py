81e09f42cd536960fdbc75a40315ade8c8ef06cd
YacoobSheriff <sheriffyacoob19@gmail.com>
2026-01-20 15:42:49 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# loops.py
+# Loop examples
+
+# for loop
+print("For loop example:")
+for i in range(1, 6):
+    print(i)
+
+# while loop
+print("While loop example:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
